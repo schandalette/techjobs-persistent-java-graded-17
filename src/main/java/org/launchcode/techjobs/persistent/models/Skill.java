71efdeaa3c1ade0807e356d0803d0801a82a5cd4
schandalette <schanda.lynn@gmail.com>
2025-02-07 17:08:21 -0600

    task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,18 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Skill() {}
 }
